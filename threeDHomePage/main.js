3ec8ed4d9a3548bc0a3a4d36d9d481da730074f3
Cordell Hammon <59060119+cumberland92@users.noreply.github.com>
2022-08-02 00:56:30 -0500

    trying to get js to work

--- threeDHomePage/main.js
+++ threeDHomePage/main.js
@@ -1,5 +1,5 @@
 //import '/style.css';
-import * as THREE from '../three.module.js';
+import * as THREE from '../threeDHomePage/three.module.js';
 //import { OrbitControls } from '/three/examples/jsm/controls/OrbitControls';
 
 // Setup
